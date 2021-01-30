6700df3dcd1ce3e2346dc6ef70a641a1ad2fd9cb
T. Rogers <tr7200@users.noreply.github.com>
2021-01-30 11:43:25 -0500

    Update Vadam.py
    update docstrings and module import formatting

--- Vadam.py
+++ Vadam.py
@@ -1,26 +1,34 @@
-# Default optimizer imports
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import six
 import copy
-import numpy as np
+import six
 from six.moves import zip
 
-from keras import backend as K
+import numpy as np
+
+from keras.optimizers import Optimizer
+from keras.legacy import interfaces
 from keras.utils.generic_utils import serialize_keras_object
 from keras.utils.generic_utils import deserialize_keras_object
-from keras.legacy import interfaces
+from keras import backend as K
 
 if K.backend() == 'tensorflow':
     import tensorflow as tf
 
-# Required for inheritance
-from keras.optimizers import Optimizer
 
 class Vadam(Optimizer):
+    """variational bayes Adam optimizer, same params as Keras Adam with
+    
+    ARGS:
+        - train_set_size (int): training dataset size
+        - prior_prec (float): prior precision (1.0 is uninformative prior)
+        - prec_init (float): precision
+        
+    RETURNS:
+        weight-perturbed gradient updates 
+    """
     def __init__(self, 
                  learning_rate=0.001,
                  train_set_size=train_set_size,
@@ -31,10 +39,12 @@ class Vadam(Optimizer):
                  **kwargs):
         self.initial_decay = kwargs.pop('decay', 0.0)
         self.epsilon = kwargs.pop('epsilon', K.epsilon())
+        
         learning_rate = kwargs.pop('lr', learning_rate)
         train_set_size = kwargs.pop('train_set_size', train_set_size)
         prior_prec = kwargs.pop('prior_prec', prior_prec)
         prec_init = kwargs.pop('prec_init', prec_init)
+        
         super(Vadam, self).__init__(**kwargs)
         with K.name_scope(self.__class__.__name__):
             self.iterations = K.variable(0, dtype='int64', name='iterations')
@@ -65,6 +75,7 @@ class Vadam(Optimizer):
               dtype=K.dtype(p),
               name='m_' + str(i))
               for (i, p) in enumerate(params)]
+        
         vs = [K.zeros(K.int_shape(p),
               dtype=K.dtype(p),
               name='v_' + str(i))
@@ -76,17 +87,22 @@ class Vadam(Optimizer):
         self.weights = [self.iterations] + ms + vs + vhats
         
         for p, g, m, v, vhat in zip(params, grads, ms, vs, vhats):
-            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g + ((self.prior_prec * p)/self.train_set_size)
+            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g + /
+                ((self.prior_prec * p)/self.train_set_size)
             v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
-            #m_t = m_t  / (1. - self.beta_1) # bias correction
-            #v_t = v_t / (1. - self.beta_2) # bias correction
-            p_t = (p + self.epsilon) - lr_t * m_t / (K.sqrt(v_t) + self.prior_prec/self.train_set_size)
+            
+            # bias correction, caused numerical instability
+            #m_t = m_t  / (1. - self.beta_1) 
+            #v_t = v_t / (1. - self.beta_2)
+            
+            p_t = (p + self.epsilon) - lr_t * m_t / (K.sqrt(v_t) + /
+                 self.prior_prec/self.train_set_size)
 
             self.updates.append(K.update(m, m_t))
             self.updates.append(K.update(v, v_t))
             new_p = p_t
 
-            # Apply constraints.
+            # Apply constraints
             if getattr(p, 'constraint', None) is not None:
                 new_p = p.constraint(new_p)
 
@@ -104,4 +120,5 @@ class Vadam(Optimizer):
                   'prior_prec': self.prior_prec,
                   'prec_init': self.prec_init}
         base_config = super(Vadam, self).get_config()
+        
         return dict(list(base_config.items()) + list(config.items()))
